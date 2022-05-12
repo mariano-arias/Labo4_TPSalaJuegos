b81fd286b49c43313e769d5492d8eb9b44b7c2f9
mariano-arias <marianoluisarias@gmail.com>
2022-05-12 09:52:59 -0300

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDjHoVyVHmg7q8DWKQymGBRrEnDVStHpg0",
+    authDomain: "crud-cd79f.firebaseapp.com",
+    projectId: "crud-cd79f",
+    storageBucket: "crud-cd79f.appspot.com",
+    messagingSenderId: "489609835301",
+    appId: "1:489609835301:web:428b93022ccc6f11d8e0cc"
+  }
 };
